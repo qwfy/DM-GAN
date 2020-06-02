8691a0eab95695828ea743b5563231a2b1577636
Incomplete <incomplete@aixon.co>
2020-06-02 11:38:26 +0800

    Some fix for the recent version of python and packages

--- code/miscc/utils.py
+++ code/miscc/utils.py
@@ -289,9 +289,10 @@ def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         #print(m.state_dict().keys())
-        if m.state_dict().keys()[0] == 'weight':
+        _inc_keys = list(m.state_dict().keys())
+        if _inc_keys[0] == 'weight':
             nn.init.orthogonal_(m.weight.data, 1.0)
-        elif m.state_dict().keys()[3] == 'weight_bar':
+        elif _inc_keys[3] == 'weight_bar':
             nn.init.orthogonal_(m.weight_bar.data, 1.0)
         #nn.init.orthogonal(m.weight.data, 1.0)
     elif classname.find('BatchNorm') != -1:
